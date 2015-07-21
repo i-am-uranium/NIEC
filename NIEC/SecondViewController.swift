7179943a4c9bc9bba1e8c940c87576a87384fadd
Ravi Ranjan <uranium@Ravis-Mac.local>
2015-07-20 23:43:09 -0700

    Initial Commit

---
+++ NIEC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NIEC
+//
+//  Created by Ravi Ranjan on 20/07/15.
+//  Copyright © 2015 Ravi Ranjan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
